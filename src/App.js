ac3caadd776d264d43181a4d0796bacc11b444b7
STR <saturasstotenkopf@gmail.com>
2020-04-01 22:04:15 +0700

    Tugas 12 Semantic UI
    mengumpulkan tugas 12 semantic ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Form, Segment, Input, Container, Checkbox, Button, Image, Header } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container style={{width:'500px'}}>
+        <center>
+          <Header as='h1' color='teal'>
+            <Image size='huge' src='https://s3-ap-southeast-1.amazonaws.com/niomic/img-v1/c_login_logo.png' />
+            <Header.Content>
+              Member Login
+            </Header.Content>
+          </Header>
+        </center>
+        <Segment stacked>
+          <Form>
+            <Input type='email' icon='user' iconPosition='left' placeholder='Email Address' fluid />
+            <br/>
+            <Input type='password' icon='lock' iconPosition='left' placeholder='Password' fluid />
+            <br/>
+            <center>
+              <Checkbox label ='I Agree Terms And Conditions'/>
+            </center>
+            <br/>
+            <Button content='Login' color='teal' fluid/>
+          </Form>
+        </Segment>
+        <Segment textAlign='center' secondary>
+          Tidak Punya Akun? Silakan <a href="/"> Register</a>
+        </Segment>
+      </Container>
     </div>
   );
 }
